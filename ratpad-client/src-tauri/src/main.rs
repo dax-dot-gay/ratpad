bc5565dd0fc36dde5a734e9e27a548f59c4bf89a
Dax Harris <mvh3875@rit.edu>
2024-08-01 12:06:05 -0400

    setup system tray and background running

--- ratpad-client/src-tauri/src/main.rs
+++ ratpad-client/src-tauri/src/main.rs
@@ -4,8 +4,14 @@
 mod util;
 
 use std::sync::Mutex;
+use tauri::CustomMenuItem;
+use tauri::SystemTray;
 
 use tauri::Manager;
+use tauri::SystemTrayEvent;
+use tauri::SystemTrayMenu;
+use tauri::SystemTrayMenuItem;
+use tauri::WindowEvent;
 use util::app_state::ApplicationState;
 use util::app_state::ConnectionState;
 pub use util::ratpad_communication;
@@ -53,6 +59,15 @@ async fn disconnect(app: tauri::AppHandle) -> Result<(), String> {
     Ok(())
 }
 
+fn generate_tray() -> SystemTray {
+    let title = CustomMenuItem::new("title".to_string(), "Ratpad Client").disabled();
+    let status = CustomMenuItem::new("status".to_string(), "Status: Disconnected").disabled();
+    let toggle = CustomMenuItem::new("toggle".to_string(), "Toggle Window");
+    let menu = SystemTrayMenu::new().add_item(title).add_item(status).add_native_item(SystemTrayMenuItem::Separator).add_item(toggle);
+
+    SystemTray::new().with_menu(menu)
+}
+
 fn main() {
     tauri::Builder::default()
         .manage(ApplicationState {
@@ -60,18 +75,63 @@ fn main() {
             port: Mutex::new(None),
             rate: Mutex::new(None),
         })
+        .plugin(tauri_plugin_single_instance::init(|app, _, _| {
+            app.trigger_global("ratpad://single-instance", None);
+        }))
         .setup(|app| {
             start_serial_listener(app);
             let handle = app.handle();
+            let evt_handle = handle.clone();
             app.listen_global("ratpad://serial", move |event| {
                 if let Some(payload) = event.payload() {
                     if let Ok(parsed) = serde_json::from_str::<SerialEvent>(payload) {
-                        handle.emit_all("ratpad://serial", parsed).expect("Serial emit failed");
+                        evt_handle.emit_all("ratpad://serial", parsed.clone()).expect("Serial emit failed");
+
+                        match parsed {
+                            SerialEvent::Connect => evt_handle.tray_handle().get_item("status").set_title("Status: Connected").unwrap(),
+                            SerialEvent::Disconnect => evt_handle.tray_handle().get_item("status").set_title("Status: Disconnected").unwrap(),
+                            SerialEvent::Event(_) => ()
+                        }
                     }
                     
                 }
             });
+            let single_instance_handle = handle.clone();
+            app.listen_global("ratpad://single-instance", move |_| {
+                single_instance_handle.get_window("main").unwrap().show().unwrap();
+            });
             Ok(())
+        })
+        .system_tray(generate_tray())
+        .on_system_tray_event(|app, event| {
+            match event {
+                SystemTrayEvent::MenuItemClick { id, .. } => {
+                    match id.as_str() {
+                        "toggle" => {
+                            if let Some(window) = app.get_window("main") {
+                                if let Ok(visible) = window.is_visible() {
+                                    if visible {
+                                        window.hide().unwrap();
+                                    } else {
+                                        window.show().unwrap();
+                                    }
+                                }
+                            }
+                        },
+                        _ => ()
+                    }
+                },
+                _ => ()
+            }
+        })
+        .on_window_event(|event| {
+            match event.event() {
+                WindowEvent::CloseRequested { api, .. } => {
+                    event.window().hide().unwrap();
+                    api.prevent_close();
+                },
+                _ => ()
+            }
         })
         .invoke_handler(tauri::generate_handler![
             list_ports,
